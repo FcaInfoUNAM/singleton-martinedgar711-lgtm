e33b2c37e88f23f82d8198a2ed61754a5d10ecf8
martinedgar711-lgtm <martinedgar711@gmail.com>
2026-01-13 18:14:06 -0600

    singleton/ AppConfig.java
    cambios guardados

---
+++ Singleton/singleton/AppConfig.java
@@ -0,0 +1,26 @@
+public class AppConfig {
+    private static final AppConfig instance = new AppConfig();
+    private String theme;
+    private String language;
+
+    private AppConfig() {
+        // Load default settings
+        this.theme = "Light";
+        this.language = "EN";
+        System.out.println("New AppConfig instance created!");
+    }
+    public static AppConfig getInstance() {
+    return instance;
+    }
+
+    public String getTheme() { return theme; }
+    public void setTheme(String theme) { this.theme = theme; }
+
+    public String getLanguage() { return language; }
+    public void setLanguage(String language) { this.language = language; }
+
+    public void printConfig() {
+        System.out.println("Theme: " + theme + ", Language: " + language);
+    }
+}
+// Este es uno de los cambios que guarde del singleton ya que el codespaces no me deja guardar los cambios
