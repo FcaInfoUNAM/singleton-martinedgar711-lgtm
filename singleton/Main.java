bbb4a780b8917d500fe12e37310aad18e60e9dd5
martinedgar711-lgtm <martinedgar711@gmail.com>
2026-01-13 18:16:13 -0600

    singleton/Main.java
    cambios guardados

---
+++ singleton/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // Different parts of the code create their own config...
+        AppConfig config1 = AppConfig.getInstance();
+        AppConfig config2 = AppConfig.getInstance();
+
+        config1.setTheme("Dark"); // Change setting in one object...
+
+        config1.printConfig();
+        config2.printConfig(); // ...but the other object is unaware! This is bad.
+
+        System.out.println("Are these the same instance? " + (config1 == config2));
+    }
+}
+//este es el cambio guardado de la actividad
